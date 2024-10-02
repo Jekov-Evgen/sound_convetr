dac7ebadbac597f7b25e6ecd8536efd3a18e4932
Jekov-Evgen <jena1245481te@gmail.com>
2024-10-02 18:15:00 +0300

    изменили отоброжение звука

--- sound.py
+++ sound.py
@@ -4,16 +4,24 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-class Sound():
+class Sound:
     def __init__(self, path_sound) -> None:
         self.path_sound = path_sound
     
     def translation(self):
-        download = librosa.load(self.path_sound)
+        y, sr = librosa.load(self.path_sound)
 
-
-        sound = librosa.stft(download)
+        sound = librosa.stft(y)
         sound_db = librosa.amplitude_to_db(abs(sound), ref=np.max)
 
-        plt.plot(sound_db)
+        mean_db = np.mean(sound_db, axis=1)
+
+        frequencies = librosa.fft_frequencies(sr=sr)
+
+        plt.figure(figsize=(10, 6))
+        plt.plot(frequencies, mean_db)
+        plt.title('Средний частотный спектр')
+        plt.xlabel('Частота (Гц)')
+        plt.ylabel('Амплитуда (дБ)')
+        plt.grid(True)
         plt.show()
